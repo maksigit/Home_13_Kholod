6403710471c55479f4c98ca83cd23ff296f1f62d
maksigit <maksi123p@gmail.com>
2017-01-21 15:40:23 +0200

    Ready

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 
 //# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
+$('.menu-icon').click(function () {
+    $('.menu').toggle();
+})
